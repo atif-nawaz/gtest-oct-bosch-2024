b859ef7bf43bda7ad4187d226002bcd49c0d5185
Atif Nawaz <68705954+atif-nawaz@users.noreply.github.com>
2024-10-29 10:57:28 +0530

    Update StringCalculatorValParameter.cpp

--- StringCalculatorValParameter.cpp
+++ StringCalculatorValParameter.cpp
@@ -20,7 +20,17 @@ public testing::WithParamInterface<tuple<string,int>>{
 
 };
 INSTANTIATE_TEST_SUITE_P(ValuesDataSet,StringCalculatorAddParameterizedFixture,testing::Values(
-  make_tuple("",0),make_tuple("0",0),make_tuple("1",1),make_tuple("1,2",3)
+  make_tuple("",0),
+  make_tuple("0",0),
+  make_tuple("1",1),
+  make_tuple("1,2",3),
+  make_tuple("1,2,3", 6),
+  make_tuple("1\n2,3",6),
+  make_tuple("//;\n1;2",3),
+  make_tuple("42,1001,3",45),
+  make_tuple("//[***]\n8***2***3",13),
+  make_tuple("//[*][%]\n4*2%3",9),
+  make_tuple("//[**][%^]\n4**1%^9",14)
 ));
 
 
@@ -31,6 +41,11 @@ TEST_P(StringCalculatorAddParameterizedFixture, DataDrivenTestCase){
     ASSERT_EQ(actualValue,expectedValue);
 }
 
+
+TEST_F(StringCalculatorAddFixture, Throw_Invalid_Argumnet_Exceptio_For_Negetive_Numbers){
+    string input="1,-2";
+    EXPECT_THROW(objUnderTest->Add(input),invalid_argument);
+  }
 
 TEST_F(StringCalculatorAddFixture, Throw_Invalid_Argumnet_Exceptio_For_Negetive_Numbers){
     string input="1,-2";
