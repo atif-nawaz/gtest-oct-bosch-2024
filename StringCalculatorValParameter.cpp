33a0e28b45f0330724d13727911ce0bf1789a47d
Atif Nawaz <68705954+atif-nawaz@users.noreply.github.com>
2024-10-29 10:51:22 +0530

    Create StringCalculatorValParameter.cpp

---
+++ StringCalculatorValParameter.cpp
@@ -0,0 +1,38 @@
+#include "stringCalculator.h"
+#include <gtest/gtest.h>
+#include <tuple>
+using namespace std;
+
+class StringCalculatorAddFixture:public testing::Test{
+  protected:
+    StringCalculator *objUnderTest;
+  void SetUp() override {
+      objUnderTest=new StringCalculator();
+  }
+  void TearDown(){
+        delete objUnderTest;
+  }
+};
+
+class StringCalculatorAddParameterizedFixture:
+public StringCalculatorAddFixture,
+public testing::WithParamInterface<tuple<string,int>>{
+
+};
+INSTANTIATE_TEST_SUITE_P(ValuesDataSet,StringCalculatorAddParameterizedFixture,testing::Values(
+  make_tuple("",0),make_tuple("0",0),make_tuple("1",1),make_tuple("1,2",3)
+));
+
+
+TEST_P(StringCalculatorAddParameterizedFixture, DataDrivenTestCase){
+    string input=std::get<0>(GetParam());
+    int expectedValue=std::get<1>(GetParam());
+    int actualValue=objUnderTest->Add(input);
+    ASSERT_EQ(actualValue,expectedValue);
+}
+
+
+TEST_F(StringCalculatorAddFixture, Throw_Invalid_Argumnet_Exceptio_For_Negetive_Numbers){
+    string input="1,-2";
+    EXPECT_THROW(objUnderTest->Add(input),invalid_argument);
+  }
